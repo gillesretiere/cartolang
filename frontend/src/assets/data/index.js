f07f3eec9b0b499b9d8fff41684d68a881224880
gillesretiere <gilles.retiere@gmail.com>
2025-07-14 13:36:15 +0000

    Retiré de .gitignore : src/assets

---
+++ frontend/src/assets/data/index.js
@@ -0,0 +1,9 @@
+import langdeck_regions from "./langdeck.regions.json";
+import langdeck_countries from "./langdeck.countries.json";
+import langdeck_languages from "./langdeck.languages.json";
+
+export {
+    langdeck_regions,
+    langdeck_countries,
+    langdeck_languages,
+};
---
+++ frontend/src/assets/img/index.js
@@ -0,0 +1,9 @@
+import hmrt_icon from "./favicon_rouge.png";
+import TravelExploreIcon from "./TravelExploreIcon.png";
+import banner_hero_languages from './ChooseLanguage.png';
+
+export {
+    hmrt_icon,
+    TravelExploreIcon,
+    banner_hero_languages,
+};
